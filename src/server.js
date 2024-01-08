a4e70d983299addf96c1a2506b842f11fd5d686e
Manuchekhr Rakhimov <30647642+ManuRakh@users.noreply.github.com>
2024-01-08 14:43:44 +0300

    Little code optimization

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 
-const port = process.env.port || 3000;
+const port = process.env.port || 3002;
 const requests = require("./modules/requests.module/routes");
 const workers = require("./modules/workers.module/routes");
 const users = require("./modules/users.module/routes");
